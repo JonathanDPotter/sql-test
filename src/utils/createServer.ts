a72753c2f06eb54f9a4c8836d9d0304df275eb9a
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-11-10 05:55:31 -0500

    Added logger and errorHandler middlewares

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import path from "path";
 import indexRoutes from "../routes/index.routes";
+import logger from "../middleware/logger";
+import errorHandler from "../middleware/errorHanlder";
 
 const createServer = () => {
   const server = express();
   server.use(express.json());
   server.use(express.static(path.join(__dirname, "../static")));
+  server.use(logger);
   server.use(indexRoutes);
+  server.use(errorHandler);
   return server;
 };
 
